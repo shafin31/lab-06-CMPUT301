147e0ea08c97f003ce805cfa0b58bc9636c9fd1b
Your Name <shafin3@ualberta.ca>
2026-02-10 17:28:43 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/listycity/City.java
@@ -0,0 +1,16 @@
+package com.example.listycity;
+
+public class City {
+    private String city;
+    private String province;
+    City(String city, String province){
+        this.city = city;
+        this.province = province;
+    }
+    String getCityName(){
+        return this.city;
+    }
+    String getProvinceName(){
+        return this.province;
+    }
+}
